4ae9a9c506b04a27f511574a750a923c5e60a528
Miguel-Ace <acevedo5198mac@gmail.com>
2025-01-16 23:32:58 -0600

    Pasando a PHP

--- add.php
+++ add.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contacts App</title>
-  <link rel="stylesheet" href="/app/css/index.css">
+  <link rel="stylesheet" href="http://localhost/PHP/contacts-app/app/css/index.css">
 </head>
 <body>
   <header>
     <div class="enlaces">
       <a href="#">ContactsApp</a>
-      <a href="./index.html">Home</a>
+      <a href="./index.php">Home</a>
       <a href="#">Add Contact</a>
     </div>
   </header>
--- index.php
+++ index.php
@@ -1,29 +1,39 @@
+<?php
+$contacts = [
+  ['name' => 'Miguel', 'phone_number' => 2220222],
+  ['name' => 'Angel', 'phone_number' => 339112],
+  ['name' => 'Eduardo', 'phone_number' => 97172]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contacts App</title>
-  <link rel="stylesheet" href="/app/css/index.css">
+  <link rel="stylesheet" href="http://localhost/PHP/contacts-app /app/css/index.css">
 </head>
 <body>
   <header>
     <div class="enlaces">
       <a href="#">ContactsApp</a>
       <a href="#">Home</a>
-      <a href="./add.html">Add Contact</a>
+      <a href="./add.php">Add Contact</a>
     </div>
   </header>
   <main class="index">
     <!-- Card de contactos -->
+    <?php foreach ($contacts as $contact) { ?>
     <div class="contacto">
-      <h3 class="nombre-contacto">Contacto 1</h3>
-      <p class="numero-contacto">202020919</p>
+      <h3 class="nombre-contacto"><?= $contact['name'] ?></h3>
+      <p class="numero-contacto"><?= $contact['phone_number'] ?></p>
       <div class="btns">
         <button class="edit-contacto">Editar</button>
         <button class="borrar-contacto">Borrar</button>
       </div>
     </div>
+    <?php } ?>
   </main>
 </body>
 </html>
