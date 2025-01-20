7d349da4efe85e6fc9f0273ca793272f2dbbca5d
Miguel-Ace <acevedo5198mac@gmail.com>
2025-01-19 23:14:26 -0600

    Conectando mi app con una db en mysql

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$database = "contacts_app";
+$user = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$host; dbname=$database;", $user, $password);
+    // foreach ($conn->query('show databases') as $row) {
+    //     print_r($row);
+    // }
+} catch (PDOException $e) {
+    die("PDO Connection Erro: " . $e->getMessage());
+}
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-  if (file_exists("contacts.json")) {
-    // $contacts = json_decode(file_get_contents('contacts.json'), true);
-    $contacts = json_decode(file_get_contents("contacts.json"), true);
-  }else{
-    $contacts = [];
-  }
+  require "database.php";
+
+  $contacts = $conn->query("SELECT * FROM contacts");
 ?>
 
 <!DOCTYPE html>
